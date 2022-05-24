8a9b7a326be05039d8e83823790a49b1aea9fbf9
aagramon <ArvinAgramon@RentReadyTest228.onmicrosoft.com>
2022-05-24 13:30:59 +0800

    Add project files.

---
+++ DataverseInteractFunctions/dto/TimeEntry.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataverseInteractFunctions.dto
+{
+    public class TimeEntry
+    {
+        public string type { get; set; }
+        public Property properties { get; set; }
+        public List<string> required { get; set; }
+    }
+
+    public class Property
+    {
+        public Attribute StartOn { get; set; }
+        public Attribute EndOn { get; set; }
+    }
+
+    public class Attribute 
+    { 
+        public string type { get; set; }
+        public string format { get; set; }
+    }
+}
